5395d9420cdf6dd6d5bf60916bb63d881319d491
Jimmy Charnley Kromann <jimmy@charnley.dk>
2026-01-14 19:42:38 +0100

    Generate speeches in mp3

--- services/video_service/src/tutorial_generator/speech_funcs.py
+++ services/video_service/src/tutorial_generator/speech_funcs.py
@@ -1,12 +1,18 @@
 import wave
+import io
 from piper import PiperVoice
+from pydub import AudioSegment
 
 def generate_audio(voice, text_script, audio_filename):
 
-    audio_filename = audio_filename.with_suffix(".wav")
-
-    with wave.open(str(audio_filename), "wb") as wav_file:
+    wav_buffer = io.BytesIO()
+    with wave.open(wav_buffer, "wb") as wav_file:
         voice.synthesize_wav(text_script, wav_file)
+    wav_buffer.seek(0)
+
+    sound = AudioSegment.from_wav(wav_buffer)
+    audio_filename = audio_filename.with_suffix(".mp3")
+    sound.export(str(audio_filename), format="mp3", bitrate="320k")
 
     return audio_filename
 
