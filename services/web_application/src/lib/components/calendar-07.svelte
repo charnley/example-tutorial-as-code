d41963ea2a508ebc058a1c890e46fc81e2b539ab
Jimmy Charnley Kromann <jimmy@charnley.dk>
2026-02-21 19:50:10 +0100

    Added some more examples

---
+++ services/web_application/src/lib/components/calendar-07.svelte
@@ -0,0 +1,21 @@
+<script>
+	import RangeCalendar from "$lib/components/ui/range-calendar/range-calendar.svelte";
+	import { CalendarDate } from "@internationalized/date";
+	let value = $state({
+		start: new CalendarDate(2025, 6, 18),
+		end: new CalendarDate(2025, 7, 7),
+	});
+</script>
+
+<div class="flex min-w-0 flex-col gap-2">
+	<RangeCalendar
+		bind:value
+		minDays={2}
+		maxDays={20}
+		numberOfMonths={2}
+		class="rounded-lg border shadow-sm"
+	/>
+	<div class="text-muted-foreground text-center text-xs">
+		Your stay must be between 2 and 20 nights
+	</div>
+</div>
---
+++ services/web_application/src/routes/calendar/+page.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import  Calendar  from "$lib/components/calendar-07.svelte";
+</script>
+
+<div class="flex min-h-svh w-full items-center justify-center p-6 md:p-10">
+
+    <Calendar />
+
+</div>
+
